31cd975d0017fa2264c9c52e9a26c12466c5bb50
Luke Babich <luke.babich@gamesglobal.com>
2026-03-24 16:21:56 +0000

    feat: split out spin runner

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    include: ['src/tests/unit/**/*.test.ts'],
+    include: ['src/tests/**/*.test.ts'],
     fileParallelism: false,
     env: {
       GAMES_JSON_PATH: 'src/data/games.test.json',
